e5283f2c321f54014b4b17011b2da8a2e4c440bd
AnasSiddiquii <sidanas1anassid.com>
2023-05-04 16:25:11 +0530

    uploaded

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,36 +1,7 @@
-from datetime import datetime
-from flask import Flask, render_template, url_for, flash, redirect
-from forms import RegistrationForm, LoginForm
-app = Flask(__name__)
-
-from flask_sqlalchemy import SQLAlchemy
-
-app.config['SECRET_KEY'] = 'ab1c7263106be15a8cd243a10b5ec8cc'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-
-db = SQLAlchemy(app)
-
-class User(db.Model):
-   id = db.Column(db.Integer, primary_key=True)
-   username = db.Column(db.String(20), unique=True, nullable=False)
-   email = db.Column(db.String(120), unique=True, nullable=False)
-   image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-   password = db.Column(db.String(60), nullable=False)
-   posts = db.relationship('Post', backref='author', lazy=True)
-
-   def __repr__(self):
-      return f"User ('{self.username}', '{self.email}', '{self.image_file}')"
-   
-class Post(db.Model):
-   id = db.Column(db.Integer, primary_key=True)
-   title = db.Column(db.String(100), nullable=False)
-   date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-   content = db.Column(db.Text, nullable=False)
-   user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-   def __repr__(self):
-      return f"User ('{self.title}', '{self.date_posted}', '{self.content}')"
-
+from flask import render_template, url_for, flash, redirect
+from flaskblog import app
+from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.models import User, Post
 
 
 posts = [
@@ -81,11 +52,6 @@ def login():
 
    return render_template('login.html', title='Login', form=form)
 
-
-
-# ? for directly running the file (python manage.py)
-# ? if __name__ == '__main__':
-# ?   app.run(debug=True)
 
 # ? html homepage
 # @app.route('/')     # ? opens the same route
